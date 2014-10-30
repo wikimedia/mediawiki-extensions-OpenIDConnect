5bb614b4efa11b6cf0b3400816670167003d13a5
cicalese <cicalese@mitre.org>
2014-10-30 15:50:12 -0400

    first commit
    Change-Id: I58aa7fcff329194157a15d1257b0555826fe623f

---
+++ OpenIDConnect.class.php
@@ -0,0 +1,246 @@
+<?php
+
+/*
+ * Copyright (c) 2014 The MITRE Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+class OpenIDConnect extends PluggableAuth {
+
+	private $subject;
+	private $issuer;
+
+	/**
+	 * @since 1.0
+	 *
+	 * @param &$id
+	 * @param &$username
+	 * @param &$realname
+	 * @param &$email
+	 */
+	public function authenticate( &$id, &$username, &$realname, &$email ) {
+
+		if ( !array_key_exists( 'SERVER_PORT', $_SERVER ) ) {
+			wfDebug( "in authenticat, server port not set" . PHP_EOL );
+			return false;
+		}
+
+		try {
+
+			if ( session_id() == '' ) {
+				wfSetupSession();
+			}
+
+			if ( isset( $_SESSION['iss'] ) ) {
+				$iss = $_SESSION['iss'];
+
+				if ( isset( $_REQUEST['code'] ) && isset( $_REQUEST['status'] ) ) {
+					unset( $_SESSION['iss'] );
+				}
+
+				if ( isset( $GLOBALS['OpenIDConnect_Config'][$iss] ) ) {
+
+					$values = $GLOBALS['OpenIDConnect_Config'][$iss];
+
+					if ( !isset( $values['clientID'] ) ||
+						!isset( $values['clientsecret'] ) ) {
+						$params = array(
+							"uri" => urlencode( $_SERVER['REQUEST_URI'] ),
+							"query" => urlencode( $_SERVER['QUERY_STRING'] )
+						);
+						self::redirect( "Special:SelectOpenIDConnectIssuer",
+							$params );
+						return false;
+					}
+
+					$clientID = $values['clientID'];
+					$clientsecret = $values['clientsecret'];
+
+				}
+
+			} else {
+
+				if ( !isset( $GLOBALS['OpenIDConnect_Config'] ) ) {
+					return false;
+				}
+
+				$iss_count = count( $GLOBALS['OpenIDConnect_Config'] );
+
+				if ( $iss_count < 1 ) {
+					return false;
+				}
+
+				if ( $iss_count == 1 ) {
+
+					$iss = array_keys( $GLOBALS['OpenIDConnect_Config'] );
+					$iss = $iss[0];
+
+					$values = array_values( $GLOBALS['OpenIDConnect_Config'] );
+					$values = $values[0];
+
+					if ( !isset( $values['clientID'] ) ||
+						!isset( $values['clientsecret'] ) ) {
+						return false;
+					}
+					$clientID = $values['clientID'];
+					$clientsecret = $values['clientsecret'];
+
+				} else {
+
+					$params = array(
+						"uri" => urlencode( $_SERVER['REQUEST_URI'] ),
+						"query" => urlencode( $_SERVER['QUERY_STRING'] )
+					);
+					$this->redirect( "Special:SelectOpenIDConnectIssuer",
+						$params );
+					return false;
+
+				}
+			}
+
+			$oidc = new OpenIDConnectClient( $iss, $clientID, $clientsecret );
+			if ( isset( $_REQUEST['forcelogin'] ) ) {
+				$oidc->addAuthParam( array( 'prompt' => 'login' ) );
+			}
+			if ( $oidc->authenticate() ) {
+
+				$username = $oidc->requestUserInfo( "preferred_username" );
+				$realname = $oidc->requestUserInfo( "name" );
+				$email = $oidc->requestUserInfo( "email" );
+				$this->subject = $oidc->requestUserInfo( 'sub' );
+				$this->issuer = $oidc->getProviderURL();
+
+				$id = $this->getId( $this->subject, $this->issuer );
+				if ( !is_null( $id ) ) {
+					return true;
+				}
+
+				if ( isset( $GLOBALS['OpenIDConnect_MigrateUsers'] ) &&
+					$GLOBALS['OpenIDConnect_MigrateUsers'] ) {
+					$id = $this->getMigratedId( $username );
+					if ( !is_null( $id ) ) {
+						$this->saveExtraAttributes( $id );
+						wfDebug( "Migrated user: " . $username );
+						return true;
+					}
+				}
+
+				$username = self::getAvailableUsername( $username );
+				return true;
+
+			} else {
+				return false;
+			}
+		} catch ( Exception $e ) {
+			return false;
+		}
+	}
+
+	/**
+	 * @since 1.0
+	 *
+	 * @param User &$user
+	 */
+	public function deauthenticate( User &$user ) {
+		if ( isset( $GLOBALS['OpenIDConnect_ForceLogout'] ) &&
+			$GLOBALS['OpenIDConnect_ForceLogout'] ) {
+			$returnto = 'Special:UserLogin';
+			$params = array( 'forcelogin' => 'true' );
+			self::redirect( $returnto, $params );
+		}
+		return true;
+	}
+
+	/**
+	 * @since 1.0
+	 *
+	 * @param $id
+	 */
+	public function saveExtraAttributes( $id ) {
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->update( 'user',
+			array( // SET
+				'subject' => $this->subject,
+				'issuer' => $this->issuer
+			), array( // WHERE
+				'user_id' => $id
+			), __METHOD__
+		);
+	}
+
+	private static function getId( $subject, $issuer ) {
+		$dbr = wfGetDB( DB_SLAVE );
+		$row = $dbr->selectRow( 'user',
+			array( 'user_id' ),
+			array(
+				'subject' => $subject,
+				'issuer' => $issuer
+			), __METHOD__
+		);
+		if ( $row === false ) {
+			return null;
+		} else {
+			return $row->user_id;
+		}
+	}
+
+	private static function getMigratedId( $username ) {
+		$nt = Title::makeTitleSafe( NS_USER, $username );
+		$username = $nt->getText();
+		$dbr = wfGetDB( DB_SLAVE );
+		$row = $dbr->selectRow( 'user',
+			array( 'user_id' ),
+			array(
+				'user_name' => $username,
+				'subject' => null,
+				'issuer' => null
+			), __METHOD__
+		);
+		if ( $row === false ) {
+			return null;
+		} else {
+			return $row->user_id;
+		}
+	}
+
+	private static function getAvailableUsername( $name ) {
+		$nt = Title::makeTitleSafe( NS_USER, $name );
+		if ( is_null( $nt ) ) {
+			$name = "User";
+		} elseif ( is_null( User::idFromName( $name ) ) ) {
+			return $nt->getText();
+		}
+		$name = $nt->getText();
+		$count = 1;
+		while ( !is_null( User::idFromName( $name . $count ) ) ) {
+			$count++;
+		}
+		return $name . $count;
+	}
+
+	public static function loadExtensionSchemaUpdates( $updater ) {
+		$updater->addExtensionField( 'user', 'subject',
+			__DIR__ . '/AddSubject.sql' );
+		$updater->addExtensionField( 'user', 'issuer',
+			__DIR__ . '/AddIssuer.sql' );
+		return true;
+	}
+}
+
---
+++ OpenIDConnect.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * Copyright (c) 2014 The MITRE Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+if ( !class_exists( 'PluggableAuth' ) ) {
+	die( '<b>Error:</b> This extension requires the PluggableAuth extension to be included first' );
+}
+
+if ( array_key_exists( 'PluggableAuth_Class', $GLOBALS ) ) {
+	die( '<b>Error:</b> A value for $PluggableAuth_Class has already been set.' );
+}
+
+$GLOBALS['wgExtensionCredits']['other'][] = array (
+	'path' => __FILE__,
+	'name' => 'OpenID Connect',
+	'version' => '1.0',
+	'author' => array(
+		'[https://www.mediawiki.org/wiki/User:Cindy.cicalese Cindy Cicalese]'
+	),
+	'descriptionmsg' => 'openidconnect-desc',
+	'url' =>
+			'https://www.mediawiki.org/wiki/Extension:OpenID_Connect',
+);
+
+$GLOBALS['PluggableAuth_Class'] = 'OpenIDConnect';
+
+$GLOBALS['wgAutoloadClasses']['OpenIDConnect'] =
+	__DIR__ . '/OpenIDConnect.class.php';
+$GLOBALS['wgAutoloadClasses']['OpenIDConnectClient'] =
+	__DIR__ . '/OpenID-Connect-PHP/OpenIDConnectClient.php5';
+
+$GLOBALS['wgMessagesDirs']['OpenIDConnect'] = __DIR__ . '/i18n';
+$GLOBALS['wgExtensionMessagesFiles']['OpenIDConnect'] =
+	__DIR__ . '/OpenIDConnect.i18n.php';
+
+$GLOBALS['wgSpecialPages']['SelectOpenIDConnectIssuer'] =
+	'SelectOpenIDConnectIssuer';
+$GLOBALS['wgAutoloadClasses']['SelectOpenIDConnectIssuer'] =
+	__DIR__ . '/SelectOpenIDConnectIssuer.class.php';
+$GLOBALS['wgExtensionMessagesFiles']['SelectOpenIDConnectIssuerAlias'] =
+	__DIR__ . '/SelectOpenIDConnectIssuer.alias.php';
+$GLOBALS['wgWhitelistRead'][] = "Special:SelectOpenIDConnectIssuer";
+
+$GLOBALS['wgHooks']['LoadExtensionSchemaUpdates'][] =
+	'OpenIDConnect::loadExtensionSchemaUpdates';
+
