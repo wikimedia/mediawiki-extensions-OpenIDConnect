b9534242b530e186a65e11d60858c2f104e68aef
cicalese <cindom@gmail.com>
2022-03-25 21:38:17 -0400

    Use HookHandlers
    Change-Id: Iba360018ec4cd366c9db0f9c1f6aaaf828ff489c
    (cherry picked from commit 9b44c5961c20df4e73583c59caa596e058d5f8c5)

--- includes/SchemaHooks.php
+++ includes/SchemaHooks.php
@@ -23,16 +23,15 @@ namespace MediaWiki\Extension\OpenIDConnect;
 
 use DatabaseUpdater;
 use FakeMaintenance;
-use MediaWiki\MediaWikiServices;
-use MediaWiki\User\UserIdentity;
+use MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook;
 
-class OpenIDConnectHooks {
+class SchemaHooks implements LoadExtensionSchemaUpdatesHook {
 	/**
-	 * Implements LoadExtensionSchemaUpdates hook.
+	 * Updates database schema.
 	 *
-	 * @param DatabaseUpdater $updater
+	 * @param DatabaseUpdater $updater database updater
 	 */
-	public static function loadExtensionSchemaUpdates( DatabaseUpdater $updater ): void {
+	public function onLoadExtensionSchemaUpdates( $updater ) {
 		$dir = $GLOBALS['wgExtensionDirectory'] . '/OpenIDConnect/sql/';
 		$type = $updater->getDB()->getType();
 		$updater->addExtensionTable( 'openid_connect', $dir . $type . '/AddTable.sql' );
@@ -59,17 +58,5 @@ class OpenIDConnectHooks {
 		} else {
 			$updater->output( '...user table does not have subject and issuer columns.' . PHP_EOL );
 		}
-	}
-
-	/**
-	 * Will populate the groups for this user with configurable properties from the access token,
-	 * if one is available for the user.
-	 * Groups will be prefixed with 'oidc_' so the plugin is able to remove them if necessary, i.e.
-	 * when a different access token is used at some other time that contains different groups.
-	 *
-	 * @param UserIdentity $user
-	 */
-	public static function populateGroups( UserIdentity $user ) {
-		MediaWikiServices::getInstance()->get( 'OpenIDConnectUserGroupManager' )->populateGroups( $user );
 	}
 }
