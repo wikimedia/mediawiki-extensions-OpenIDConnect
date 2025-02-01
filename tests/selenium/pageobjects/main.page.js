94be89b18c2e01d0bde38e9ab0a9a04d9ab02aa5
Cindy Cicalese <cindom@gmail.com>
2025-02-01 15:56:19 -0500

    Refactor Keycloak selenium test support into separate class
    Change-Id: I5d902a31b6f2d58355def5227adc3539b5ec7dc6

--- tests/selenium/pageobjects/main.page.js
+++ tests/selenium/pageobjects/main.page.js
@@ -8,34 +8,12 @@ class MainPage extends Page {
 		return $( '#pt-login-2 a' );
 	}
 
-	get usernameField() {
-		return $( '#username' );
-	}
-
-	get passwordField() {
-		return $( '#password' );
-	}
-
-	get kcFormLogin() {
-		return $( '#kc-form-login' );
-	}
-
-	get kcLogin() {
-		return $( '#kc-login' );
-	}
-
 	get userpage() {
 		return $( '#pt-userpage-2 span' );
 	}
 
 	async open() {
 		return super.openTitle( 'Main Page' );
-	}
-
-	async login( username, password ) {
-		await this.usernameField.setValue( username );
-		await this.passwordField.setValue( password );
-		await this.kcLogin.click();
 	}
 }
 
